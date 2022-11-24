fe7230708af9e1eedadc5ed00f37bfafa55f6579
mcolas7 <m.colas@sapalomera.cat>
2022-11-24 13:38:44 +0100

    database migrations

---
+++ database/migrations/2022_11_24_121413_sexes.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Sexes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sexes', function (Blueprint $table) {
+            $table->bigIncrements('sex_id');
+            $table->string('name',10);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sexes');
+    }
+}
---
+++ database/migrations/2022_11_24_121419_sizes.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Sizes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sizes', function (Blueprint $table) {
+            $table->bigIncrements('size_id');
+            $table->string('name',10);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sexes');
+    }
+}
